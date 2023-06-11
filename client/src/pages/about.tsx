7dbd0c38ced03de0011a2a436a8a956d5438fb9d
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2023-06-11 20:25:29 +0700

    feat: added basic layout

---
+++ client/src/pages/about.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import { Container, Typography } from "@mui/material";
+
+export default function About() {
+  return (
+    <>
+      <Container className="align-center">
+        <Typography variant="h1" sx={{ mt: 20 }}>
+          About
+        </Typography>
+      </Container>
+    </>
+  );
+}

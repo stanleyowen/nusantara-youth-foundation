971f361dac822e081d0b6aa1c76b208b487e0367
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2023-06-16 22:16:36 +0700

    feat: optimized code base quality

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
 import { Container, Typography } from "@mui/material";
 
 export default function Contact() {

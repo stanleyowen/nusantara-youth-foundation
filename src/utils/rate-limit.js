aaa74e4adf6a9ae08a6545246e5a87c663a87aaf
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2023-06-16 11:48:03 +0700

    fix: extracts `ip` from `X-Forwarded-For` when `ip` is `undefined`

--- src/utils/rate-limit.js
+++ src/utils/rate-limit.js
@@ -3,18 +3,40 @@ const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   max: 30, // 30 requests
-  message:
-    "We're sorry, but you have made too many requests to our servers. Please try again later.",
+  keyGenerator: (req) => {
+    return (
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.ip
+    );
+  },
+  handler: (_, res) => {
+    res.status(429).send(
+      JSON.stringify(
+        {
+          statusCode: 429,
+          statusMessage: "Too many requests",
+          message: "Too many requests, please try again later.",
+        },
+        null,
+        2
+      )
+    );
+  },
 });
 
 module.exports = (req, res, next) => {
   limiter(req, res, (err) => {
     if (err) {
-      return res
-        .status(529)
-        .send(
-          "We're sorry, but you have made too many requests to our servers. Please try again later."
-        );
+      res.status(429).send(
+        JSON.stringify(
+          {
+            statusCode: 429,
+            statusMessage: "Too many requests",
+            message: "Too many requests, please try again later.",
+          },
+          null,
+          2
+        )
+      );
     } else next();
   });
 };

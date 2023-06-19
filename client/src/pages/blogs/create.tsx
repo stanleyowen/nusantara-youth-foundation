a0db96c43195ac972eab694d7def8b714051a5b7
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2023-06-19 20:32:02 +0700

    feat: added `/create` path for blogs

---
+++ client/src/pages/blogs/create.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, FormEvent } from "react";
+import {
+  Box,
+  Grid,
+  Divider,
+  CssBaseline,
+  TextField,
+  Typography,
+  Container,
+} from "@mui/material";
+import axios from "axios";
+import { LoadingButton } from "@mui/lab";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useAuthContext } from "@/firebase/auth";
+
+export default function CreateBlog() {
+  const [isLoading, setStatus] = useState<boolean>(true);
+
+  const router = useRouter();
+  const { user }: any = useAuthContext();
+
+  useEffect(() => {
+    if (user?.email)
+      axios.get(`/admin/${user?.email}`).then((res) => {
+        if (res.data?.email === user?.email && res.data?.isAdmin)
+          setStatus(false);
+        else router.push("/blogs");
+      });
+    else router.push("/blogs");
+  }, [user]);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Create Blog
+        </Typography>
+        {/* <CreateBlogForm /> */}
+      </Box>
+    </Container>
+  );
+}
